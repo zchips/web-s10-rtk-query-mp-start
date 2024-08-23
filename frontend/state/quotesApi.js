9d3b8251a6e5a0aa3039c1360d5e8a658ff0bd51
zchips <zchips@gmail.com>
2024-08-22 18:26:11 -0600

    wip

--- frontend/state/quotesApi.js
+++ frontend/state/quotesApi.js
@@ -1,1 +1,16 @@
 // create your RTK Query endpoints here
+import {creatApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const quotesApi = createApi({
+    reducerPath: 'quotesAPi',
+    endpoints: builder =>({
+        getQuotes: builder.query({}),
+        createQuote: builder.query({}),
+        toggleQuote: builder.query({}),
+        deleteQuote: builder.query({})
+    })
+})
+
+export const {
+    useGetQuotesQuery, useToggleFakeMutation, useCreateQuoteMutation, useDeleteQuoteMutation,
+} = quotesApi
--- frontend/state/store.js
+++ frontend/state/store.js
@@ -1,8 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
 import quotesReducer from './quotesSlice'
+import {quotesApi} from './quotesApi'
+
 
 export const store = configureStore({
   reducer: {
     quotesState: quotesReducer,
-  }
+    [quotesApi.reducerPath]: quotesApi.reducer,
+  },
+
+  middleware: getDefault => getDefault().concat(
+    quotesApi.middleware
+  )
 })
